01a79343eed82a0219045840bd741b3c9466b681
Linus Modin <limo0223@storsjogymnasiet.se>
2023-03-27 13:52:33 +0200

    mer kod

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import bil 
 
+looping = True
+
+
 Ford_red = bil.Bil("Ford", "Röd", 5)
 
 print(Ford_red.getFabrikat())
 
+
+while looping == True:
+    print("----------------------------------------------------------------")
+    print("BilAutomat")
+
+    svar = input("\n Vill du avsluta programmet? j/n :")
+    if (svar == "j"):
+        break
